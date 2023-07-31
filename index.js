f20d4ba9030cba3d5e48af510bae36cee548e3dc
Alycia <alyekaka@gmail.com>
2023-07-31 11:20:46 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,20 @@
+import cipher from './cipher.js';
+
+console.log(cipher);
+
+function encrypt() {
+  const originalText = document.getElementById("originalText").value;
+  const shift = parseInt(document.getElementById("shift").value);
+  const resultText = cipher.ceasarCipher(originalText, shift);
+  document.getElementById("resultText").value = resultText;
+}
+
+function decrypt() {
+  const originalText = document.getElementById("originalText").value;
+  const shift = parseInt(document.getElementById("shift").value);
+  const resultText = cipher.ceasarCipher(originalText, 26 - shift); 
+  document.getElementById("resultText").value = resultText;
+}
+
+document.getElementById("botaoencryp").addEventListener ("click",encrypt);
+document.getElementById("botaodescrypt").addEventListener ("click",decrypt);
