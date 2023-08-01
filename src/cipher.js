53a58a59b3e1137a3afe5009e593bcadbf228abc
Alycia <alyekaka@gmail.com>
2023-08-01 14:13:09 -0300

    cifra

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,20 @@
 export default{
-  ceasarCipher:
-    function (text, shift) {
+  encode:
+    function (shift, text) {
+      if(typeof shift !== 'number' || typeof text !== "string"){
+        throw new TypeError("Key precisa ser um número");
+      }
       let result = "";
-
+      if (shift < 0 ) {
+        shift = shift + 26
+      }
       for (let i = 0; i < text.length; i++) {
         let char = text[i];
   
         if (char.match(/[a-z]/i)) {
           const code = text.charCodeAt(i);
-  
-          if (code >= 65 && code <= 90) {
+                
+          if (code >= 65 && code <= 90) { 
             char = String.fromCharCode(((code - 65 + shift) % 26) + 65);
           } else if (code >= 97 && code <= 122) {
             char = String.fromCharCode(((code - 97 + shift) % 26) + 97);
@@ -19,6 +24,8 @@ export default{
       }
       return result;
     },
-
+  decode: function(shift, text) {
+    return this.encode(26 - shift, text)
+  }
 };
     
--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 function encrypt() {
-  const originalText = document.getElementById("originalText").value;
-  const shift = parseInt(document.getElementById("shift").value);
-  const resultText = cipher.ceasarCipher(originalText, shift);
-  document.getElementById("resultText").value = resultText;
+  const originalText = document.getElementById("originalText").value;  /* box de digitar a mensagem*/
+  const shift = parseInt(document.getElementById("shift").value);      /* botao de deslize do valor*/
+  const resultText = cipher.encode(shift, originalText);         /* resultado puxando do cipher.js*/
+  document.getElementById("resultText").value = resultText;            /* lançando para o box do resultado*/
 }
 
 function decrypt() {
-  const originalText = document.getElementById("originalText").value;
-  const shift = parseInt(document.getElementById("shift").value);
-  const resultText = cipher.ceasarCipher(originalText, 26 - shift); 
-  document.getElementById("resultText").value = resultText;
+  const originalText = document.getElementById("originalText").value;  /* pegando o que foi escrito no encript*/
+  const shift = parseInt(document.getElementById("shift").value);      /* pegando o valor do off set*/
+  const resultText = cipher.decode (shift, originalText);    /* realizando o calculo*/
+  document.getElementById("resultText").value = resultText;            /* resultado*/
 }
 
 document.getElementById("botaoencryp").addEventListener ("click",encrypt);
-document.getElementById("botaodescrypt").addEventListener ("click",decrypt);
+document.getElementById("botaodescrypt").addEventListener ("click",decrypt);  /* botoes */
